2cc572fe11685a3826ce536466507795472c1d55
SilverCard <ricardo.b@msn.com>
2015-05-21 09:06:00 -0300

    Refatoração.

---
+++ NFeSharp/Esquemas/Namespaces.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFeSharp.Esquemas
+{
+    public static class Namespaces
+    {
+        public const String NFe = "http://www.portalfiscal.inf.br/nfe";
+    }
+}
---
+++ NFeSharp/Servicos/NFeDistribuicaoDFe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace NFeSharp.Servicos
+{
+    
+    [WebServiceBinding(Name = "NFeDistribuicaoDFeSoap", Namespace = "http://www.portalfiscal.inf.br/nfe/wsdl/NFeDistribuicaoDFe")]
+    internal class NFeDistribuicaoDFe : SoapHttpClientProtocol
+    {
+
+        [SoapDocumentMethod("http://www.portalfiscal.inf.br/nfe/wsdl/NFeDistribuicaoDFe/nfeDistDFeInteresse", RequestNamespace = "http://www.portalfiscal.inf.br/nfe/wsdl/NFeDistribuicaoDFe", ResponseNamespace = "http://www.portalfiscal.inf.br/nfe/wsdl/NFeDistribuicaoDFe", Use = System.Web.Services.Description.SoapBindingUse.Literal, ParameterStyle = System.Web.Services.Protocols.SoapParameterStyle.Wrapped)]
+        public System.Xml.XmlNode nfeDistDFeInteresse(System.Xml.XmlNode nfeDadosMsg)
+        {
+            object[] results = this.Invoke("nfeDistDFeInteresse", new object[] {
+                    nfeDadosMsg});
+            return ((System.Xml.XmlNode)(results[0]));
+        }
+    }
+}
