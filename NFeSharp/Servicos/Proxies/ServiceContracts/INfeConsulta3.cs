6321a0bc2f50a51b5abc766ac24be1119ad9f6ac
SilverCard <ricardo.b@msn.com>
2015-05-26 10:12:12 -0300

    .

---
+++ NFeSharp/Servicos/Proxies/ServiceContracts/INfeConsulta3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFeSharp.Servicos.Proxies
+{
+
+    [ServiceContractAttribute(Namespace = "http://www.portalfiscal.inf.br/nfe/wsdl/NfeConsulta3")]
+    public interface INfeConsulta3
+    {     
+        [OperationContract(Action = "http://www.portalfiscal.inf.br/nfe/wsdl/NfeConsulta3/nfeConsultaNF", ReplyAction = "*")]
+        [XmlSerializerFormat(SupportFaults=true)]
+        nfeConsultaNFResponse nfeConsultaNF(nfeConsultaNFRequest request);
+
+        [OperationContract(Action = "http://www.portalfiscal.inf.br/nfe/wsdl/NfeConsulta3/nfeConsultaNF", ReplyAction = "*")]
+        Task<nfeConsultaNFResponse> nfeConsultaNFAsync(nfeConsultaNFRequest request);
+    }
+
+}
